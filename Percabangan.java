7f066e44d08d4f7b1fc517fdb43ded8d068b6aac
Endah Sriwahyuni <67082771+Endah-Sriwahyuni@users.noreply.github.com>
2020-06-30 17:49:03 +0700

    Add files via upload
    Tugas Pertemuan 2

---
+++ Percabangan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Percabangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bilangan = 51;
+        
+        if(bilangan%2==0){
+            System.out.println("Bilangan Genap");
+        }else
+            System.out.println("Bilangan Ganjil");
+    }
+}
